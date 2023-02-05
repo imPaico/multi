71b8b9773e8f34b560793d53929f735f666e6ac3
apico <andrea.pico@astir.com>
2023-02-05 15:17:26 +0100

    removes mysql2 js files and adds create schema for mysql

--- multi-backend/repository/userRepository.js
+++ multi-backend/repository/userRepository.js
@@ -1,16 +1,12 @@
-const baseRepository = require('./baseRepository');
-const config = require('../config');
 const { PrismaClient } = require("@prisma/client")
 
 async function getUsers(page = 1){
   try {
       const prisma = new PrismaClient();
-      console.log(prisma.user);
       const results = await prisma.user.findMany();
       return results;
   } catch (error) {
       console.error('userRepository Error:' + error);
-      //return res.status(500).json({ message: error.message });
   }
 }
 
