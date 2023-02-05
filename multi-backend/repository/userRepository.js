11f7d6d1fb2b0ba7713141a2e14d5dbceec97f28
apico <andrea.pico@astir.com>
2023-02-05 14:50:54 +0100

    Adds prisma library to execute crud operations for mysql

--- multi-backend/repository/userRepository.js
+++ multi-backend/repository/userRepository.js
@@ -1,19 +1,17 @@
 const baseRepository = require('./baseRepository');
 const config = require('../config');
+const { PrismaClient } = require("@prisma/client")
 
 async function getUsers(page = 1){
-  //determina offset di pagine da estrarre
-  const offset = baseRepository.getOffset(page, config.listPerPage);
-  //query
-  const rows = await baseRepository.query(
-    `SELECT id_user, name
-    FROM user LIMIT ${offset},${config.listPerPage}`
-  );
-
-  //const data = baseRepository.emptyOrRows(rows);
-  console.log('userRepository:' + rows);
-  return rows;
-  
+  try {
+      const prisma = new PrismaClient();
+      console.log(prisma.user);
+      const results = await prisma.user.findMany();
+      return results;
+  } catch (error) {
+      console.error('userRepository Error:' + error);
+      //return res.status(500).json({ message: error.message });
+  }
 }
 
 module.exports = {
