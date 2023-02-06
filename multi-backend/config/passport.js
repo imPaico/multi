ad8fa4c2ddd873bbd16e54ce1af481d45e7b7840
imPaico <andreapico1998@yahoo.it>
2023-02-06 22:07:11 +0100

    separates passport js initialization in a different file.

---
+++ multi-backend/config/passport.js
@@ -0,0 +1,49 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+
+var initializePassport = function(verify) {
+
+    /* Configure password authentication strategy.
+    *
+    * The `LocalStrategy` authenticates users by verifying a username and password.
+    * The strategy parses the username and password from the request and calls the
+    * `verify` function.
+    *
+    * The `verify` function queries the database for the user record and verifies
+    * the password by hashing the password supplied by the user and comparing it to
+    * the hashed password stored in the database.  If the comparison succeeds, the
+    * user is authenticated; otherwise, not.
+    */
+    passport.use(new LocalStrategy({usernameField: "email"}, verify));
+
+    /* Configure session management.
+    *
+    * When a login session is established, information about the user will be
+    * stored in the session.  This information is supplied by the `serializeUser`
+    * function, which is yielding the user ID and username.
+    *
+    * As the user interacts with the app, subsequent requests will be authenticated
+    * by verifying the session.  The same user information that was serialized at
+    * session establishment will be restored when the session is authenticated by
+    * the `deserializeUser` function.
+    *
+    * Since every request to the app needs the user ID and username, in order to
+    * fetch todo records and render the user element in the navigation bar, that
+    * information is stored in the session.
+    */
+    passport.serializeUser(function(user, cb) {
+        process.nextTick(function() {
+            console.log(`serializing user: ${user.email}`);
+            cb(null, {email: user.email });
+        });
+    });
+
+    passport.deserializeUser(function(user, cb) {
+        console.log(`deserializing user: ${user.email}`);
+        process.nextTick(function() {
+            return cb(null, user);
+        });
+    });
+}
+
+module.exports = initializePassport
--- multi-backend/routes/auth.js
+++ multi-backend/routes/auth.js
@@ -4,73 +4,35 @@ var LocalStrategy = require('passport-local');
 var crypto = require('crypto');
 var router = express.Router();
 var userRepository = require('../repository/userRepository');
+var initializePassport = require('../config/passport');
 
-/* Configure password authentication strategy.
- *
- * The `LocalStrategy` authenticates users by verifying a username and password.
- * The strategy parses the username and password from the request and calls the
- * `verify` function.
- *
- * The `verify` function queries the database for the user record and verifies
- * the password by hashing the password supplied by the user and comparing it to
- * the hashed password stored in the database.  If the comparison succeeds, the
- * user is authenticated; otherwise, not.
+/**
+ * Initialization of passport js
  */
-passport.use(new LocalStrategy({usernameField: "email"},
-    async function verify(email, password, done) {
-        try {
-            let data = await userRepository.findUserEmailAndPassword(email, password); 
-            console.log(`auth - passport verfication - data extracted: ${data.email}`);
-            if (data == null) {
-                done(err);
-            }
-            crypto.pbkdf2(password, data.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-                if (err) { return cb(err); }
-                if (!crypto.timingSafeEqual(data.password, hashedPassword)) {
-                return done(null, false, { message: 'Incorrect username or password.' });
-                }
-                console.log('auth - passport verfication success! -  ' + data);
-                return done(null, data);
-            });
-        } catch (err) {
-            console.error(`Errore durante l'estrazione degli utenti da db: `, err.message);
-            done(null, false, {message: err.message});
+initializePassport(async function verify(email, password, done) {
+    try {
+        let data = await userRepository.findUserEmailAndPassword(email, password); 
+        console.log(`auth - passport verfication - data extracted: ${data.email}`);
+        if (data == null) {
+            done(err);
         }
-}));
-
-/* Configure session management.
- *
- * When a login session is established, information about the user will be
- * stored in the session.  This information is supplied by the `serializeUser`
- * function, which is yielding the user ID and username.
- *
- * As the user interacts with the app, subsequent requests will be authenticated
- * by verifying the session.  The same user information that was serialized at
- * session establishment will be restored when the session is authenticated by
- * the `deserializeUser` function.
- *
- * Since every request to the app needs the user ID and username, in order to
- * fetch todo records and render the user element in the navigation bar, that
- * information is stored in the session.
- */
-passport.serializeUser(function(user, cb) {
-    process.nextTick(function() {
-        console.log(`serializing user: ${user}`);
-      cb(null, { id: user.id, username: user.username });
-    });
-  });
-  
-passport.deserializeUser(function(user, cb) {
-    console.log(`deserializing user: ${user.email}, ${user.password}`);
-    process.nextTick(function() {
-        return cb(null, user);
-    });
+        crypto.pbkdf2(password, data.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+            if (err) { return cb(err); }
+            if (!crypto.timingSafeEqual(data.password, hashedPassword)) {
+            return done(null, false, { message: 'Incorrect username or password.' });
+            }
+            console.log('auth - passport verfication success! -  ' + data);
+            return done(null, data);
+        });
+    } catch (err) {
+        console.error(`Errore durante l'estrazione degli utenti da db: `, err.message);
+        done(null, false, {message: err.message});
+    }
 });
 
-/* GET home page. */
+/* GET login. */
 router.get('/login', async function(req, res, next) {
     try {
-      //res.json(await userRepository.getUsers(1)); 
       res.send({
         message: 'Home page!',
         csrfToken: req.csrfToken()
@@ -146,9 +108,15 @@ router.post('/signup', function(req, res, next) {
     });
 });
 
-/* GET home page. */
+/* home page. */
 router.get('/home', function(req, res, next) {
     res.send('home page! Logged in')
 });
+
+/* home page. */
+router.get('/application', function(req, res, next) {
+    res.send('application page!')
+});
+
 
 module.exports = router;
